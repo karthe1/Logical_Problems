d97c97ccf5cd4fe5f25fb0f8fb4455dbb9dda2f3
karthickamala <42179012+karthickamala@users.noreply.github.com>
2019-09-24 16:42:06 +0530

    Solved hacker earth problem in formatting the date.

---
+++ src/com/karthick/solve/FormatDate.java
@@ -0,0 +1,46 @@
+package com.karthick.solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+
+/**
+ * 
+ * @author karthick.r
+ *
+ */
+public class FormatDate {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int noOftestCases = Integer.parseInt(br.readLine());
+		ArrayList<String> dateList = new ArrayList<String>();
+		while (noOftestCases > 0) {
+			dateList.add(br.readLine());
+			noOftestCases--;
+		}
+		Iterator<String> itr = dateList.iterator();
+		while (itr.hasNext()) {
+			String inputDateString = itr.next();
+			String splitDate[] = inputDateString.split(" ");
+			if ((Integer.parseInt(splitDate[0]) > 0)
+					&& ((Integer.parseInt(splitDate[2]) >= 1600) && (Integer.parseInt(splitDate[2]) <= 2000))) {
+				Date inputDate = new Date(inputDateString);
+				inputDate.setDate(inputDate.getDate() - 1);
+				SimpleDateFormat sdf2 = new SimpleDateFormat("dd MMMM YYYY");
+				String outputFormat = sdf2.format(inputDate);
+				System.out.println(outputFormat);
+			} else {
+				System.err.append("INVALID DATE OR INVALID YEAR (RANGES FROM 1600 TO 2000)");
+			}
+		}
+
+	}
+
+}
